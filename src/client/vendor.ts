6e3b06d90aa802e9936f0d897df48c89d30894e8
Adam Yost <swimmadude66@gmail.com>
2018-03-25 00:33:03 -0400

    home-rolled scss flexbox grid!

--- src/client/vendor.ts
+++ src/client/vendor.ts
@@ -1,8 +1,5 @@
 import 'reflect-metadata/Reflect';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
-import 'bootstrap';
-import 'jquery';
-import 'popper.js';
 
 import 'rxjs/Rx';
